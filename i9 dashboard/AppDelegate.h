cce918e5907441c6fa46b43c90cd9665bd6d3eda
Rudney de Camargo <rudneycamargo@brq.com>
2018-03-14 10:09:49 -0300

    Initial Commit

---
+++ i9 dashboard/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  i9 dashboard
+//
+//  Created by Rudney Camargo Pereira on 14/03/18.
+//  Copyright © 2018 Rudney Camargo Pereira. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ i9 dashboard/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  i9 dashboard
+//
+//  Created by Rudney Camargo Pereira on 14/03/18.
+//  Copyright © 2018 Rudney Camargo Pereira. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
