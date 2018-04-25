8ad5f77550361ebe050f4053fd6cfa79d8ccbb16
Rudney de Camargo <rudneycamargo@brq.com>
2018-04-25 11:38:04 -0300

    importante menu hambúrguer

---
+++ i9 dashboard/FatherDashboard/Controller/MainNavigation/SWRevealViewControllerShield.m
@@ -0,0 +1,21 @@
+//
+//  SWRevealViewControllerShield.m
+//  aco
+//
+//  Created by Leandro Luizari on 02/12/16.
+//  Copyright © 2016 itau. All rights reserved.
+//
+
+#import "SWRevealViewControllerShield.h"
+
+@implementation SWRevealViewControllerShield
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
