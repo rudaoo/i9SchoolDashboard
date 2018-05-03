fd25271d2f35afb80451a3917d4c752407236a76
Rudney de Camargo <rudneycamargo@brq.com>
2018-05-03 17:02:01 -0300

    animacao de aviao

--- i9 dashboard/Controllers/SonDash/Dash/SonDashboardViewController.m
+++ i9 dashboard/Controllers/SonDash/Dash/SonDashboardViewController.m
@@ -9,6 +9,7 @@
 #import "SonDashboardViewController.h"
 
 @interface SonDashboardViewController ()
+@property (weak, nonatomic) IBOutlet UIImageView *airplaneImage;
 
 @end
 
@@ -35,6 +36,9 @@
                                         nil]
                               forState:UIControlStateNormal];
     self.navigationItem.rightBarButtonItem = buttonItem2;
+    
+    
+    [self planeDepart];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -43,6 +47,48 @@
 }
 
 -(void) onButtonPopViewController{
+}
+
+-(void) planeDepart {
+    
+    CGPoint originalCenter = self.airplaneImage.center;
+    UIImageView *backupAirplane = self.airplaneImage;
+    
+    
+    [UIView animateKeyframesWithDuration:1.5 delay:0 options:UIViewKeyframeAnimationOptionRepeat animations:^{
+    
+        [UIView addKeyframeWithRelativeStartTime:0 relativeDuration:0.25 animations:^{
+            
+            [self.airplaneImage setFrame:CGRectMake(self.airplaneImage.center.x + 80, self.airplaneImage.center.y - 50, self.airplaneImage.frame.size.width, self.airplaneImage.frame.size.height) ];
+        }];
+        
+        [UIView addKeyframeWithRelativeStartTime:0.1 relativeDuration:0.8 animations:^{
+            
+            self.airplaneImage.transform = CGAffineTransformMakeRotation(-M_PI/2);
+        }];
+        
+        [UIView addKeyframeWithRelativeStartTime:0.25 relativeDuration:0.25 animations:^{
+            
+            [self.airplaneImage setFrame:CGRectMake(self.airplaneImage.center.x + 100, self.airplaneImage.center.y - 200, self.airplaneImage.frame.size.width, self.airplaneImage.frame.size.height) ];
+            
+            [self.airplaneImage setAlpha:0];
+        }];
+        
+        [UIView addKeyframeWithRelativeStartTime:0.51 relativeDuration:0.1 animations:^{
+            
+            self.airplaneImage.transform = CGAffineTransformMakeRotation(0);
+            [self.airplaneImage setFrame:CGRectMake(0, originalCenter.y, self.airplaneImage.frame.size.width, self.airplaneImage.frame.size.height) ];
+        }];
+        
+        [UIView addKeyframeWithRelativeStartTime:0.55 relativeDuration:0.45 animations:^{
+            
+            [self.airplaneImage setAlpha:1];
+            [self.airplaneImage setCenter:originalCenter];
+        }];
+        
+    
+    
+    }completion:nil];
 }
 
 @end
